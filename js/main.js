c5ba3414eceb28d50f2d288dea2fee726232936a
dlgudwnsgpffh <59990737+dlgudwnsgpffh@users.noreply.github.com>
2020-06-15 00:17:30 +0900

    모바일 터치이벤트 추가

--- js/main.js
+++ js/main.js
@@ -31,3 +31,32 @@ function handleMouseUp(evt){
 
 cropper.addEventListener('mousedown', handleMouseDown);
 document.addEventListener('mouseup', handleMouseUp);
+
+
+
+// Mobile의 경우 터치
+
+function handleTouchMove(evt){
+    movePositionX=evt.touches[0].clientX
+    movePositionY=evt.touches[0].clientY
+    let x=movePositionX-positionX;
+    let y=movePositionY-positionY;
+    if(x>=0&&x<=200&&y>=0&&y<=160){
+        cropper.style.transform=`translate(${x}px, ${y}px)`;
+        innerImg.style.transform=`translate(${-x}px, ${-y}px)`;
+    }
+}
+
+function handleTouchStart(evt){
+    positionX=evt.touches[0].clientX;
+    positionY=evt.touches[0].clientY;
+    document.addEventListener('touchmove', handleTouchMove);
+}
+
+function handleTouchEnd(evt){
+    document.removeEventListener('touchmove', handleTouchMove);
+}
+
+
+cropper.addEventListener('touchstart', handleTouchStart);
+document.addEventListener('touchend', handleTouchEnd);
