3bc62c954451fd05d7eaaaa521b88c460a8035c8
kokocan12 <49009864+kokocan12@users.noreply.github.com>
2020-06-18 17:22:40 +0900

    cropper 드래그 버그 수정

--- js/main.js
+++ js/main.js
@@ -1,57 +1,117 @@
 const cropper=document.querySelector('#cropper');
 const innerImg=document.querySelector('#inner-img');
 const innerImg2=document.querySelector('#inner-img2');
+const seDot=document.querySelector('#se-dot');
+const innerCropper=document.querySelector('#inner-cropper');
 
-let positionX;
-let positionY;
 
-let movePositionX;
-let movePositionY;
+let startPositionX;
+let startPositionY;
+
+let endPositionX;
+let endPositionY;
+let x=0;
+let y=0;
+
+let cropperBorderX=200;
+let cropperBorderY=160;
 
 function handleMouseMove(evt){
-    movePositionX=evt.clientX;
-    movePositionY=evt.clientY;
-    let x=movePositionX-positionX;
-    let y=movePositionY-positionY;
-    if(x>=0&&x<=200&&y>=0&&y<=160){
-        cropper.style.transform=`translate(${x}px, ${y}px)`;
-        innerImg.style.transform=`translate(${-x}px, ${-y}px)`;
-        innerImg2.style.transform=`translate(${-x}px, ${-y}px)`;
+    endPositionX=evt.clientX;
+    endPositionY=evt.clientY;
+    x+=(endPositionX-startPositionX);
+    y+=(endPositionY-startPositionY);
+    if(x<0){
+        x=0;
+    } else if(x>cropperBorderX){
+        x=cropperBorderX;
     }
+
+    if(y<0){
+        y=0;
+    } else if(y>cropperBorderY){
+        y=cropperBorderY;
+    }
+    cropper.style.transform=`translate(${x}px, ${y}px)`;
+    innerImg.style.transform=`translate(${-x}px, ${-y}px)`;
+    innerImg2.style.transform=`translate(${-x}px, ${-y}px)`;
+    startPositionX=endPositionX;
+    startPositionY=endPositionY;
 }
 
 function handleMouseDown(evt){
-    positionX=evt.clientX;
-    positionY=evt.clientY
+    startPositionX=evt.clientX;
+    startPositionY=evt.clientY
     document.addEventListener('mousemove', handleMouseMove);
 }
 
 function handleMouseUp(evt){
     document.removeEventListener('mousemove', handleMouseMove);
+    document.removeEventListener('mousemove', mouseMoveSeDot);
+}
+
+function mouseDownSeDot(evt){
+    evt.stopPropagation();
+    startPositionX=evt.clientX;
+    startPositionY=evt.clientY;
+    document.addEventListener('mousemove', mouseMoveSeDot);
+}
+
+function mouseMoveSeDot(evt){
+    endPositionX=evt.clientX;
+    let currentCropperWidth=(Number)(cropper.style.width.slice(0,-2));
+
+    
+    currentCropperWidth+=(endPositionX-startPositionX);
+    // width값이 변하는 범위에 제한 필요
+    if((x+currentCropperWidth)<=300&&(y+currentCropperWidth*1.4)<=300){
+        cropper.style.width=`${currentCropperWidth}px`;
+        innerCropper.style.width=`${currentCropperWidth}px`;
+        // width값이 변하면 height값은 비례하여 변함. x1.4
+        cropper.style.height=`${currentCropperWidth*1.4}px`;
+        innerCropper.style.height=`${currentCropperWidth*1.4}px`;
+    }
+    console.log(currentCropperWidth);
+    // cropper 이동값 제한 설정
+    cropperBorderX=300-currentCropperWidth;
+    cropperBorderY=300-(currentCropperWidth*1.4);
+    startPositionX=endPositionX;
 }
 
 cropper.addEventListener('mousedown', handleMouseDown);
 document.addEventListener('mouseup', handleMouseUp);
+seDot.addEventListener('mousedown' ,mouseDownSeDot)
 
 
 
 // Mobile의 경우 터치
 
 function handleTouchMove(evt){
-    movePositionX=evt.touches[0].clientX
-    movePositionY=evt.touches[0].clientY
-    let x=movePositionX-positionX;
-    let y=movePositionY-positionY;
-    if(x>=0&&x<=200&&y>=0&&y<=160){
-        cropper.style.transform=`translate(${x}px, ${y}px)`;
-        innerImg.style.transform=`translate(${-x}px, ${-y}px)`;
-        innerImg2.style.transform=`translate(${-x}px, ${-y}px)`;
+    endPositionX=evt.touches[0].clientX;
+    endPositionY=evt.touches[0].clientY;
+    x+=(endPositionX-startPositionX);
+    y+=(endPositionY-startPositionY);
+    if(x<0){
+        x=0;
+    } else if(x>200){
+        x=200;
     }
+
+    if(y<0){
+        y=0;
+    } else if(y>160){
+        y=160;
+    }
+    cropper.style.transform=`translate(${x}px, ${y}px)`;
+    innerImg.style.transform=`translate(${-x}px, ${-y}px)`;
+    innerImg2.style.transform=`translate(${-x}px, ${-y}px)`;
+    startPositionX=endPositionX;
+    startPositionY=endPositionY;
 }
 
 function handleTouchStart(evt){
-    positionX=evt.touches[0].clientX;
-    positionY=evt.touches[0].clientY;
+    startPositionX=evt.touches[0].clientX;
+    startPositionY=evt.touches[0].clientY;
     document.addEventListener('touchmove', handleTouchMove);
 }
 
